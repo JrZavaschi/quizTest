d1d9ea6d6a49d4b5b706d37bd79f0d3e2d55c618
Olinoir Junior Zavaschi <jrzavaschi@outlook.com>
2018-08-27 00:58:42 -0300

    idx

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Escalasoft</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/png" href="view/tecnologia/img/favicon.png" />
+<meta http-equiv="refresh" content="0; url=view/quiz_select.php" />
+<body>
+</body>
+</html>
